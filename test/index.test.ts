22fa1eb08744dc70ac01c5c9251c5de149bde167
pyros2097 <peter@pyros.sh>
2021-04-03 09:20:27 +0530

    fix lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,10 @@
 import { atom } from '../src';
 
 describe('atom', () => {
-
   it('getValue', () => {
     const atomOne = atom(10);
     const atomTwo = atom(20);
-    const sumAtom = atom((get) => get(atomOne) + get(atomTwo));
+    const sumAtom = atom(get => get(atomOne) + get(atomTwo));
     expect(atomOne.getValue()).toEqual(10);
     expect(atomTwo.getValue()).toEqual(20);
     expect(sumAtom.getValue()).toEqual(30);
@@ -13,27 +12,24 @@ describe('atom', () => {
 
   it('update', () => {
     const atomOne = atom(10);
-    const sumAtom = atom((get) => get(atomOne) + 5);
-    atomOne.update((v) => v + 2)
+    const sumAtom = atom(get => get(atomOne) + 5);
+    atomOne.update(v => v + 2);
     expect(atomOne.getValue()).toEqual(12);
     expect(sumAtom.getValue()).toEqual(17);
-  })
+  });
 
   it('subscribe', async () => {
     const atomOne = atom(10);
-    const sumAtom = atom((get) => get(atomOne) + 5);
-    atomOne.subscribe((newValue) => expect(newValue).toEqual(15));
-    sumAtom.subscribe((newValue) => expect(newValue).toEqual(20));
-    atomOne.update((v) => v + 5)
-  })
+    const sumAtom = atom(get => get(atomOne) + 5);
+    atomOne.subscribe(newValue => expect(newValue).toEqual(15));
+    sumAtom.subscribe(newValue => expect(newValue).toEqual(20));
+    atomOne.update(v => v + 5);
+  });
 
   // it('useAtom', async () => {
   // })
-
 });
 
-describe('asyncAtom', () => {
-})
+describe('asyncAtom', () => {});
 
-describe('usePromise', () => {
-})
+describe('usePromise', () => {});
